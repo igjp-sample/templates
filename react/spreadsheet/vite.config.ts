1f09c018769f61136601b83c50a4a8de8a155743
jsakamotoIGJP <jsakamoto@infragistics.com>
2024-08-07 14:41:21 +0900

    Update the vite configuration to use random TCP ports

--- react/spreadsheet/vite.config.ts
+++ react/spreadsheet/vite.config.ts
@@ -1,15 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	define: process.env.VITEST ? {} : { global: 'window' },
-	plugins: [react()],
-	resolve: {
-		mainFields: ['module'],
-	},
-	server: {
-		open: true,
-		port: 3000
-	}
-})
+  define: process.env.VITEST ? {} : { global: "window" },
+  plugins: [react()],
+  resolve: {
+    mainFields: ["module"],
+  },
+  server: {
+    open: true,
+  },
+});
