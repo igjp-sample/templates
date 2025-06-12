4ce469650d1fb0de6bd64cce7926e0a39c4c449a
jsakamotoIGJP <jsakamoto@infragistics.com>
2025-06-12 14:28:09 +0900

    Update the Grid template for Angular to support Angular v20

--- ng/grid/src/app/app.config.ts
+++ ng/grid/src/app/app.config.ts
@@ -1,12 +1,12 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, Provider } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideAnimations } from '@angular/platform-browser/animations';
 
 import { routes } from './app.routes';
 
-export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideAnimations()]
-};
+const providers: Provider = [
+  provideRouter(routes),
+  provideAnimations()
+];
+
+export const appConfig: ApplicationConfig = { providers };
